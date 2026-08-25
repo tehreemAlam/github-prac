45acc5b160cdfd524ab5a9fa03eb27269c0113d3
Tehreem Alam <tehreemalam90@gmail.com>
2026-08-25 17:40:15 +0500

    Update and rename testing.py to testing.py

---
+++ testing.py
@@ -0,0 +1,9 @@
+expenses = []
+
+expense = input("Enter an expense: ")
+amount = float(input("Enter the amount: "))
+
+expenses.append([expense, amount])
+
+print("Expense:", expense)
+print("Amount:", amount)
